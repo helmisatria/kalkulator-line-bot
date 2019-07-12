6c38d1ea2594359c138cbc211182a2e312777d0e
Helmi Satria <satriahelmi@gmail.com>
2019-07-12 16:24:17 +0700

    fix: dynamic port

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ bot.onEvent(async context => {
 
 const server = createServer(bot);
 
-server.listen(5000, () => {
-  console.log("server is running on 5000 port...");
+server.listen(process.env.PORT || 5000, () => {
+  console.log("server is running on port 5000...");
 });
